65fef799ceed681e05f738c31a648a4a217ce576
johan210lumw <105176582+johan210lumw@users.noreply.github.com>
2025-07-10 01:18:54 -0600

    Update etl_script.py

--- app_spark/scripts/etl_script.py
+++ app_spark/scripts/etl_script.py
@@ -1,34 +1,14 @@
 from pyspark.sql import SparkSession
 
-# output is annoying me
-import logging
-# Set logging level
-logging.getLogger("org").setLevel(logging.ERROR)
-logging.getLogger("akka").setLevel(logging.ERROR)
+spark = SparkSession.builder.appName("ETL").getOrCreate()
 
-# Initialize Spark session
-spark = SparkSession.builder \
-    .appName("ETL Script") \
-    .master("spark://spark:7077") \
-    .getOrCreate()
+# Extract
+df = spark.read.parquet("/app/data/yellow_tripdata_2025-01.parquet")
 
-# Read the Parquet file
-input_path = "/app/data/yellow_tripdata_2025-01.parquet"
-df = spark.read.parquet(input_path)
+# Transform
+df_clean = df.dropna().dropDuplicates()
 
-# Example transformation: Filter for rides with a passenger count greater than 1
-transformed_df = df.filter(df.passenger_count > 1)
+# Load
+df_clean.write.parquet("/opt/spark-app/output", mode="overwrite")
 
-# Write the transformed data to the Druid-shared volume
-#output_path = "/app/druid_shared/yellow_tripdata_transformed.parquet"
-output_path = "/app/data/yellow_tripdata_transformed.parquet"
-# check if data can be written
-if transformed_df.count() > 0:
-    transformed_df.write.parquet(output_path)
-else:
-    print("No data to write.")
-# Write the thing please god please
-transformed_df.write.parquet(output_path)
-
-# Stop the Spark session
 spark.stop()
